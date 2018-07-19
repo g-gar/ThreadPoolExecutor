31b1fc098aca0db25085a215cdb4ec31ba608db1
g-gar <g.gar.fern@hotmail.com>
2018-07-19 12:11:18 +0200

    20180719

--- src/com/ggar/dev/test/threadpoolexecutor/factorial/Factorial.java
+++ src/com/ggar/dev/test/threadpoolexecutor/factorial/Factorial.java
@@ -3,20 +3,30 @@ package com.ggar.dev.test.threadpoolexecutor.factorial;
 import java.util.Scanner;
 
 import com.ggar.dev.test.threadpoolexecutor.factorial.implementations.FactorialOperation;
-import com.ggar.dev.test.threadpoolexecutor.factorial.implementations.FactorialOperationPool;
 import com.ggar.dev.test.threadpoolexecutor.factorial.implementations.FactorialSingleton;
 import com.ggar.dev.threadpoolexecutor.Main;
+import com.ggar.dev.threadpoolexecutor.Singleton.Singleton;
 import com.ggar.dev.threadpoolexecutor.executor.Executor;
 import com.ggar.dev.threadpoolexecutor.executor.GenericExecutor;
 import com.ggar.dev.threadpoolexecutor.operation.Operation;
+import com.ggar.dev.threadpoolexecutor.pool.GenericPool;
 import com.ggar.dev.threadpoolexecutor.pool.Pool;
 
 public class Factorial implements Main  {
+	
+	private Singleton singleton;
+	private Pool pool;
+	private Executor executor;
+	
+	public Factorial() {
+		singleton = new FactorialSingleton();
+		pool = new GenericPool<FactorialOperation>();
+		executor = new GenericExecutor(pool);
+	}
+	
+	@Override
 	public void run() {
-		new FactorialSingleton();
 		Scanner sc = new Scanner(System.in);
-		Pool<FactorialOperation> pool = new FactorialOperationPool();
-		Executor executor = new GenericExecutor(pool);
 		String line;
 		
 		try {
@@ -26,7 +36,7 @@ public class Factorial implements Main  {
 				int number = Integer.parseInt(line);
 				Operation<Integer> op = new FactorialOperation(number);
 				if (!op.equals(null))
-					pool.add((FactorialOperation) op);
+					pool.add(op);
 			}
 			executor.stop();
 			sc.close();
@@ -34,4 +44,5 @@ public class Factorial implements Main  {
 			e.printStackTrace();
 		}
 	}
+	
 }
--- src/com/ggar/dev/test/threadpoolexecutor/factorial/implementations/FactorialSingleton.java
+++ src/com/ggar/dev/test/threadpoolexecutor/factorial/implementations/FactorialSingleton.java
@@ -3,38 +3,72 @@ package com.ggar.dev.test.threadpoolexecutor.factorial.implementations;
 import java.math.BigInteger;
 
 import com.ggar.dev.threadpoolexecutor.Singleton.Singleton;
+import com.ggar.dev.threadpoolexecutor.memoizer.ConcurrentMemoizer;
 import com.ggar.dev.threadpoolexecutor.memoizer.Memoizer;
 
 public class FactorialSingleton extends Singleton<Integer, BigInteger> {
 
-	private Memoizer<Integer, BigInteger> memo = new FactorialMemoizer();
+	private Memoizer<Integer, BigInteger> memo = new ConcurrentMemoizer<Integer, BigInteger>(){};
 	
 	public FactorialSingleton() {
-		this.configure();
-	}
-	
-	@Override
-	protected void configure() {
-		this.INSTANCE = this;
+		this.configure(this);
 	}
 	
 	@Override
 	public BigInteger du(Integer value) {
-		BigInteger result;
+		BigInteger result = BigInteger.ZERO;
 		if (!this.memo.isComputed(value)) {
 			System.out.println("Computing " + value + "!...");
-			result = this.computeFactorial(value);
-			this.memo.set(value, result);
+			try {
+				result = this.compute(value);
+			} catch (Exception e) {
+				System.out.println("catch body");
+				e.printStackTrace();
+			} finally {
+				this.memo.set(value, result);
+			}
 		} else {
 			result = this.memo.get(value);
 		}
 		return result;
 	}
 
-	private BigInteger computeFactorial(Integer n) {
-		return computeFactorial(n, BigInteger.valueOf(1));
+	private BigInteger compute(Integer n) {
+		BigInteger result = BigInteger.ONE;
+		//TODO compute knowing the maximum value computed for a better approach
+		int i = 0; //TODO compute max recursion depth || number of recursion calls made by function
+		int j = n ; //TODO compute step size for corecursion
+		
+		for (int k = 0; k - j < n; k += j) {
+			Integer min = k - j;
+			min = (min < 0) ? 0 : min;
+			BigInteger temp = compute(k, min, result);
+			result = result.equals(BigInteger.ONE) ? temp : result.multiply(temp);
+		}
+		return result;
 	}
-	private BigInteger computeFactorial(Integer n, BigInteger accumulator) {
-		return memo.isComputed(n) ? memo.get(n) : (n <= 1 ? accumulator : computeFactorial(n - 1, accumulator.multiply(BigInteger.valueOf(n))));
+	private BigInteger compute(Integer n, BigInteger accumulator) {
+		return compute(n, 1, accumulator);
+//		return memo.isComputed(n) ? memo.get(n) : (n <= 1 ? accumulator : compute(n - 1, accumulator.multiply(BigInteger.valueOf(n))));
+	}
+	private BigInteger compute(Integer nmax, Integer nmin, BigInteger accumulator) {
+//		if (memo.isComputed(nmax)) {
+//			return memo.get(nmax);
+//		} else if (nmax > nmin) {
+//			if (nmax == 0)
+//				return BigInteger.ONE;
+//			else if (nmax == 1 || nmax.equals(nmin + 1)) 
+//				return accumulator;
+//			else
+//				return compute(nmax - 1, nmin, accumulator.multiply(BigInteger.valueOf(nmax)));
+//		} else {
+//			return accumulator;
+//		}
+		if (!memo.isComputed(nmax)) {
+			for (; nmax > 0 && nmax > nmin; nmax--) {
+				accumulator = accumulator.multiply(BigInteger.valueOf(nmax));
+			}
+		} else accumulator = memo.get(nmax);
+		return accumulator;
 	}
 }
